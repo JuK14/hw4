8a88335c294bbe5e6979839dbdb48d0eb5cc66dc
JuK14 <julialex1407@gmail.com>
2022-09-04 22:19:32 +0400

    Написана вторая программа

--- Program.cs
+++ Program.cs
@@ -9,3 +9,19 @@ for (int i = 1; i<=b; i++)
     count = count*a;
 }
 Console.WriteLine(count);
+
+
+
+// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+Console.WriteLine("Введите число ");
+int a = int.Parse(Console.ReadLine());
+int count = 0;
+while (a != 0)
+{
+    int LastDig = a%10;
+    count = count + LastDig;
+    a = a/10;
+
+}
+Console.WriteLine("Сумма цифр в этом числе равна " + count);
+
