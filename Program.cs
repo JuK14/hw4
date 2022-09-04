28cbe3b23516c552e6513204e7bd00a0d5a1d076
JuK14 <julialex1407@gmail.com>
2022-09-04 22:30:21 +0400

    Написана третья программа

--- Program.cs
+++ Program.cs
@@ -25,3 +25,32 @@ while (a != 0)
 }
 Console.WriteLine("Сумма цифр в этом числе равна " + count);
 
+
+
+
+// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while(index<length)
+    {
+        collection[index] = new Random().Next(0,100);
+        index++;
+    }
+}
+void PrintArray(int [] coll)
+{
+    int count = coll.Length;
+    int pos = 0;
+    Console.Write("[ ");
+    while (pos < count)
+    {
+        Console.Write(coll[pos] + " ");
+        pos++;
+    }
+    Console.Write("]");
+}
+int [] array = new int[8];
+FillArray (array);
+PrintArray (array);
